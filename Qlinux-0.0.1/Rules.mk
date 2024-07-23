12dd73629bcfe088f67c9a5c724fafca812178a5
qiuyixiang <qiuyixiang2003@126.com>
2024-07-23 18:46:28 +0800

    version 0.0.1

---
+++ Qlinux-0.0.1/Rules.mk
@@ -0,0 +1,10 @@
+# This Makefile Define All of the Build Tool Chain And File Path
+
+# Define Global Path
+
+GLOBAL_INCLUDE	?=	./inclue
+GLOBAL_BUILD	?=	./build
+GLOBAL_X86		?=	./x86
+GLOBAL_KERNEL	?=	./kernel
+GLOBAL_DRIVER	?=	./driver
+
